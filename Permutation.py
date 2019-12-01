c3d3bc7f512db5696ec1589d13b372eb46187a41
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2019-12-01 10:05:17 +0530

    Create Permutation.py

---
+++ Permutation.py
@@ -0,0 +1,17 @@
+n,k=input().split()
+N=int(n)
+h=int(k)
+K=N-h
+value=1
+sums=1
+if N<=10000 and K<=10000 or (N-K)<5:
+        while(N>0):
+            value=value*N
+            N=N-1
+        l=value
+        while(K>0):
+            sums=sums*K
+            K=K-1
+        m=sums
+        c=int(l/m)
+        print(c)
