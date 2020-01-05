762bded3c252796049448fb97a49e599597454b8
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2020-01-05 13:29:53 +0530

    Create Sum_of_diagonals.py

---
+++ Sum_of_diagonals.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=[]
+for i in range(n):
+        h=[int(x) for x in input().split()][:2]
+        l.append(h)
+k=0
+o=0
+for i in range(n):
+        for j in range(n):
+                if (i==j):
+                        k=k+l[i][j]
+                if ((i+j)==(n-1)):
+                        o=o+l[i][j]
+print(k*o)
