a42d42883339f4831785d6cb481f4e5710de9416
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2022-11-16 19:38:42 +0530

    Update loops_in_JSON.js

--- task-3/loops_in_JSON.js
+++ task-3/loops_in_JSON.js
@@ -1,31 +1,35 @@
 // for loop in JSON
-var jsons=[{name:'David',age:22,address:'Madhavaram',phone:8237465}];
-for (var i=0; i<jsons.length;i++){
-    var obj=jsons[i];
-    for (var val in obj){
-        console.log(val+':'+obj[val]);
+var jo={"person": {"name": "Alice","country": "Austria","tasks": ["develop", "design", "test"],"age": 30}};
+var jso=[jo.person];
+for (var i=0;i<jso.length;i++){
+    var val=jso[i]
+    console.log(val);
+    for(var j in val){
+        console.log(j+":"+val[j]);
     }
 }
 
 // for in loop in JSON
-var jsons={name:'David',age:22,address:'Madhavaram',phone:8237465};
-for (var val in jsons){
-    console.log(val+":"+jsons[val]);
+var jo={"person": {"name": "Alice","country": "Austria","tasks": ["develop", "design", "test"],"age": 30}};
+var jso=jo.person;
+for (val in jso){
+    console.log(val+":"+jso[val]);
 }
 
 //for of loop in JSON
-var jsons=[{name:'David',age:22,address:'Madhavaram',phone:8237465}];
-for(var ele of jsons){
-    for (var val in ele){
-        console.log(val+':'+ele[val]);
+var jo={"person": {"name": "Alice","country": "Austria","tasks": ["develop", "design", "test"],"age": 30}};
+var jso=[jo.person];
+for (var i of jso){
+    for(var j in i){
+        console.log(j+":"+i[j]);
     }
 }
 
 //for each loop in JSON
-var jsons=[{name:'David',age:22,address:'Madhavaram',phone:8237465}];
-jsons.forEach(funct=>{
-    for (var i=0; i<jsons.length;i++){
-        var obj=jsons[i];
-        for (var val in obj){
-            console.log(val+':'+obj[val]);}}
+var jo={"person": {"name": "Alice","country": "Austria","tasks": ["develop", "design", "test"],"age": 30}};
+var jso=[jo.person];
+jso.forEach(fuctt=>{
+    for (var val in fuctt){
+        console.log(val+":"+fuctt[val]);
+    }
 })
